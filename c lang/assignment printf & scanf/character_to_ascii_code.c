42f43ab869fc4683da56a05ac6027b83f15fd081
sankalp kumar <sankalp05oct@gmail.com>
2024-01-15 23:16:10 +0530

    three character to its corosponding asciii code

---
+++ c lang/assignment printf & scanf/character_to_ascii_code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int  main(void) {
+
+
+    char x,y,z;
+	printf("enter the three character's to gets its ascii:  ");
+	scanf("%c,%c,%c",&x,&y,&z);
+	printf("the ascii code character '%c' = %d, '%c' = %d, '%c' = %d",x,x,y,y,z,z);
+	printf("\n ");
+	return 0;
+}
