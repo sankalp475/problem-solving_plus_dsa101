8fa12a9408cbd0e6badcda92ec60711377e068ca
sankalp kumar <sankalp05oct@gmail.com>
2024-01-15 22:49:18 +0530

    character to ascii

---
+++ c lang/assignment printf & scanf/Ascii_to_character.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int  main(void) {
+
+    int acsii_code;
+	printf("Enter the <acsii_code> to get corosponding character: ");
+	scanf("%d",&acsii_code);
+    printf(" the ascii code is %d and character is %c\n",acsii_code,acsii_code);
+	printf("\n");
+	return 0;
+}
