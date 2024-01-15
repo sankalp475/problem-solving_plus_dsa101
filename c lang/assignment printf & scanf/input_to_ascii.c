b2a36975310d0e418eb8f9af8ce41553b0b500c4
sankalp kumar <sankalp05oct@gmail.com>
2024-01-15 22:39:47 +0530

    input to ascii number

---
+++ c lang/assignment printf & scanf/input_to_ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+	char ch;
+	printf("enter the number to find its <ascii code>: ");
+	scanf("%c",&ch);
+
+	printf("the ascii value of char %c is %d",ch,ch);
+	printf("\n");
+	return 0;
+}
